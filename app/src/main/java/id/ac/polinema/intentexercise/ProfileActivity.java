e1b3d65a886d3f3cf9b09ce10c854bebf32eada6
amelrnt <ochaarn@gmail.com>
2020-02-23 21:14:41 +0700

    add profile page data

--- app/src/main/java/id/ac/polinema/intentexercise/ProfileActivity.java
+++ app/src/main/java/id/ac/polinema/intentexercise/ProfileActivity.java
@@ -3,12 +3,40 @@ package id.ac.polinema.intentexercise;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+    private TextView fullname;
+    private TextView aboutMe;
+    private TextView email;
+    private TextView homepage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        fullname = findViewById(R.id.label_fullname);
+        aboutMe = findViewById(R.id.label_about);
+        email = findViewById(R.id.label_email);
+        homepage = findViewById(R.id.label_homepage);
+
+        Bundle extras = getIntent().getExtras();
+
+        if (extras!=null){
+            String name = extras.getString(RegisterActivity.FULLNAME_KEY);
+            String about = extras.getString(RegisterActivity.ABOUTYOU_KEY);
+            String mail = extras.getString(RegisterActivity.EMAIL_KEY);
+            String homePage = extras.getString(RegisterActivity.HOMEPAGE_KEY);
+
+            fullname.setText(name);
+            aboutMe.setText(about);
+            email.setText(mail);
+            homepage.setText(homePage);
+        }
+
+
+
     }
+
+
 }
--- app/src/main/java/id/ac/polinema/intentexercise/RegisterActivity.java
+++ app/src/main/java/id/ac/polinema/intentexercise/RegisterActivity.java
@@ -2,13 +2,104 @@ package id.ac.polinema.intentexercise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.ConfirmPassword;
+import com.mobsandgeeks.saripaar.annotation.Domain;
+import com.mobsandgeeks.saripaar.annotation.Email;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
+import com.mobsandgeeks.saripaar.annotation.Password;
+
+import java.util.List;
+
+public class RegisterActivity extends AppCompatActivity implements Validator.ValidationListener {
+    @NotEmpty
+    private EditText fullname;
+    @NotEmpty
+    @Domain
+    private EditText homepage;
+    @NotEmpty
+    private EditText aboutYou;
+    @NotEmpty
+    @Email
+    private EditText email;
+    @Password
+    private EditText password;
+    @ConfirmPassword
+    private EditText confirmPassword;
+    protected Validator validator;
+    private ImageView gb1;
+    private static final int GALLERY_REQUEST_CODE = 1;
+    public static final String FULLNAME_KEY = "username";
+    public static final String EMAIL_KEY = "aaa@gmail.com";
+    public static final String PASSWORD_KEY = "1234";
+    public static final String ABOUTYOU_KEY = "asdfghjkl";
+    public static final String HOMEPAGE_KEY = "polinema.ac.id";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        validator = new Validator(this);
+        validator.setValidationListener(this);
+        gb1 = findViewById(R.id.image_profile);
+        fullname = findViewById(R.id.text_fullname);
+        email = findViewById(R.id.text_email);
+        homepage = findViewById(R.id.text_homepage);
+        password = findViewById(R.id.text_password);
+        confirmPassword = findViewById(R.id.text_confirm_password);
+        aboutYou = findViewById(R.id.text_about);
+        homepage = findViewById(R.id.text_homepage);
+    }
+
+    public void handleSave(View view) {
+        validator.validate();
+        validator.setValidationMode(Validator.Mode.IMMEDIATE);
+        Intent intent = new Intent(this, ProfileActivity.class);
+        String namaLengkap = fullname.getText().toString();
+        String mail = email.getText().toString();
+        intent.putExtra(FULLNAME_KEY,namaLengkap);
+        intent.putExtra(EMAIL_KEY,mail);
+        intent.putExtra(PASSWORD_KEY,password.getText().toString());
+        intent.putExtra(PASSWORD_KEY,confirmPassword.getText().toString());
+        intent.putExtra(ABOUTYOU_KEY,aboutYou.getText().toString());
+        intent.putExtra(HOMEPAGE_KEY,homepage.getText().toString());
+        startActivity(intent);
+    }
+
+    @Override
+    public void onValidationSucceeded() {
+
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(this);
+
+            // Display error messages ;)
+            if (view instanceof EditText) {
+                ((EditText) view).setError(message);
+            } else {
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public void handlePhoto(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, GALLERY_REQUEST_CODE);
+
     }
 }
+
